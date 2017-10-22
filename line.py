5f897a4ed67d7c45d7b595c88828d70194aeece5
milkhamil <32932970+milkhamil@users.noreply.github.com>
2017-10-22 22:53:12 +0700

    Update line.py

--- line.py
+++ line.py
@@ -1688,6 +1688,32 @@ def bot(op):
 					cl.sendMessage(msg)
 					cl.sendMessage(msg)
 					
+            elif msg.text in ["Mention"]:
+                  group = cl.getGroup(msg.to)
+                  nama = [contact.mid for contact in group.members]
+
+                  cb = ""
+                  cb2 = ""
+                  strt = int(0)
+                  akh = int(0)
+                  for md in nama:
+                      akh = akh + int(6)
+
+                      cb += """{"S":"""+json.dumps(str(strt))+""","E":"""+json.dumps(str(akh))+""","M":"""+json.dumps(md)+"},"""
+
+                      strt = strt + int(7)
+                      akh = akh + 1
+                      cb2 += "@nrik \n"
+
+                  cb = (cb[:int(len(cb)-1)])
+                  msg.contentType = 0
+                  msg.text = cb2
+                  msg.contentMetadata ={'MENTION':'{"MENTIONEES":['+cb+']}','EMTVER':'4'}
+
+                  try:
+                      cl.sendMessage(msg)
+                  except Exception as error:
+                      print error
   #-------------Fungsi Tagall---------------#
             elif msg.text in ["Tagall"]:
 				if msg.from_ in admin:
